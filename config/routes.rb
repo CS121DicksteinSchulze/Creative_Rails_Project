c94ff2184a472cedc06b40911a3f0c41ef9d3490
Thomas Dickstein <thomas.dickstein@pomona.edu>
2018-02-11 19:55:35 -0800

    cleaned up unused files

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'output.html' => 'welcome#output'
   get 'input.html.erb' => 'welcome#index'
-  post 'output.php' => 'welcome#output'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
