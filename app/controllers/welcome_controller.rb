5063b43a2ffcf0de0699be6da31bd32d87c4f78c
Thomas Dickstein <thomas.dickstein@pomona.edu>
2018-02-11 19:52:45 -0800

    enabled form information to be sent as parameters to bot

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,30 @@
 class WelcomeController < ApplicationController
   def index
   end
+  def output
+		@input = params[:message]
+		@output = ""
+		respond
+
+	end
+
+	def respond
+		if (@input.include? "hello")
+			@output = "Howdy!"
+		elsif (@input.include? "hi")
+			@output = "Hello!"
+		elsif (@input.include? "how are you")
+			@output = "I am here for you"
+		elsif (@input.include? "music")
+			@output = "I usually listen to myself sing."	
+		elsif (@input.include? "song")
+			@output = "I usually listen to myself sing."
+		elsif (@input.include? "movie")
+			@output = "Anything with Dwayne The Rock Johnson."
+		elsif (@input.include? "bye")
+			@output = "Farewell!"
+		else
+			@output = "Sorry, I didn't understand. Hope you're having a nice day."
+		end
+	end
 end
